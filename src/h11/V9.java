54ee6172c6ce2764d477ccbb5a385d66f0de3ccc
xbranw <bwoolley@roc-dev.com>
2019-10-10 08:49:51 +0200

    hoofstuk 11 v9

---
+++ src/h11/V9.java
@@ -0,0 +1,139 @@
+package h11;
+
+import java.awt.*;
+import java.applet.*;
+
+public class V9 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        int y = 50;
+        int x = 50;
+        int hoogte = 50;
+        int breedte = 50;
+
+        for (int kolom = 0; kolom < 8; kolom++) {
+
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+        }
+        // TWEEDE(2) RIJ
+        x = 50;
+        y += hoogte;
+
+        for (int kolom = 0; kolom < 8; kolom++) {
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+        }
+        // DERDE(3) RIJ
+        x = 50;
+        y += hoogte;
+
+        for (int kolom = 0; kolom < 8; kolom++) {
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+        }
+        // VIERDE(4) RIJ
+        x = 50;
+        y += hoogte;
+
+        for (int kolom = 0; kolom < 8; kolom++) {
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+        }
+        // VIJFDE (5) RIJ
+        x = 50;
+        y += hoogte;
+
+        for (int kolom = 0; kolom < 8; kolom++) {
+
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+        }
+        // ZESDE(6) RIJ
+        x = 50;
+        y += hoogte;
+
+        for (int kolom = 0; kolom < 8; kolom++) {
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+        }
+        // ZEVENDE(7) RIJ
+        x = 50;
+        y += hoogte;
+
+        for (int kolom = 0; kolom < 8; kolom++) {
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+        }
+        // ACHTSTE(8) RIJ
+        x = 50;
+        y += hoogte;
+
+        for (int kolom = 0; kolom < 8; kolom++) {
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+        }
+
+
+    }
+}
